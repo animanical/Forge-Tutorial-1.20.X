5df8e7643b5b727e90ecc572882a67313139aa51
Ani <annaliisa@rogers.com>
2026-01-03 18:01:26 -0500

    Added an item!

--- src/main/java/net/animanical/tutorialmod/TutorialMod.java
+++ src/main/java/net/animanical/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.animanical.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.animanical.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -25,6 +27,8 @@ public class TutorialMod {
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -37,7 +41,9 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)  {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SHINE_STONE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
